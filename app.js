9286fde9fd153f4082c17300d2aa6b9fdb50e799
Prateek Reddy <prateekreddyy@gmail.com>
2021-06-05 16:50:50 +0530

    cleanup var names

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const config = require("./config/config.json");
 const Data = require(`./data`);
-const simulationMain = require(`./data`)["simulation"]
 var tradingDecision = require("./decisions")
 
 console.log(config.simulation)
@@ -18,7 +17,7 @@ async function init(){
     await dataInstance.initialFetch();
 
     let tradingAlgo = tradingDecision[config.algorithm];
-    let tradingObj = new tradingAlgo(config, dataInstance);
+    let algoInstance = new tradingAlgo(config, dataInstance);
 
     const algoConf = config.algoConfig[config.algorithm];
     if(config.simulation){
@@ -26,8 +25,8 @@ async function init(){
         var traderObj = new Trader(currency1 = 0, currency2 = 1, conversion = queryObj.getcurrentPrice())
         for(var i = 0; i < algoConf.fetchConfig.nTest-algoConf.lastNCandles-1; i++){
             console.log("start")
-            await tradingObj.updateCandles()
-            var tradeVal = await tradingObj.whatToDo()
+            await algoInstance.updateCandles()
+            var tradeVal = await algoInstance.whatToDo()
             console.log("action : ",tradeVal,"\n\n")
             await traderObj.trade(tradeVal)
 
@@ -36,7 +35,7 @@ async function init(){
         return;
     }
     console.log("ledger details : ",traderObj.ledger)
-    tradeObj = await tradingObj.whatToDo()
+    tradeObj = await algoInstance.whatToDo()
 }
 
 init()
